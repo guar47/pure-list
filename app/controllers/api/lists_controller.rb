7e289bcdd43abf97fe2064011f798dac140808ef
Dmitrii Pashutskii <dpashutskii@gmail.com>
2019-07-30 21:55:14 +0200

    add api back, add react, few more gems

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,7 @@
+class Api::ListsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with List.all
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  
+  self.responder = ApplicationResponder
+  respond_to :html
+
 end
